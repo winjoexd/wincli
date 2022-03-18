45c31f8f8ef06d6cfc693c2dee6345236b9449d6
winjoexd <winjoexd@gmail.com>
2022-03-18 16:40:45 +0800

    add processbar

--- src/main.rs
+++ src/main.rs
@@ -4,6 +4,7 @@ use clap::Parser;
 use std::io::prelude::*;
 use std::io::BufReader;
 use std::fs::File;
+use indicatif::ProgressBar;
 
 #[derive(Parser)]
 struct Cli {
@@ -17,10 +18,15 @@ fn main() -> std::io::Result<()> {
    
     println!("pattern = {}", args.pattern);
     println!("path = {:?}", args.path);
-    println!("===========================");
 
     let f = File::open(&args.path)?;
     let reader = BufReader::new(f);
+
+    let pb = indicatif::ProgressBar::new(100);
+    for i in 0..100 {
+        pb.inc(1);
+    }
+    pb.finish();
     
     for line in reader.lines() {
         let l = line.as_ref().unwrap();
