e23160423e80544501958bf45cb43e6954fe5285
winjoexd <winjoexd@gmail.com>
2023-07-10 23:41:03 +0800

    test hello bin

--- hello/tests/cli.rs
+++ hello/tests/cli.rs
@@ -1,9 +1,8 @@
-use std::process::Command;
+use assert_cmd::Command;
 
 #[test]
 fn runs(){
-    let mut cmd = Command::new("ls");
-    let res = cmd.output();
+    let mut cmd = Command::cargo_bin("hello").unwrap();
 
-    assert!(res.is_ok());
+    cmd.assert().success();
 }
